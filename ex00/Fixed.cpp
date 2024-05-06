39065a9b100a74306e0268938314f75537e021ba
nojia1775 <nojia.adjemrose@gmail.com>
2024-05-06 23:56:38 +0200

    ex00

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,37 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed(void)
+{
+	this->fixed_point = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &other)
+{
+	this->fixed_point = other.fixed_point;
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed&	Fixed::operator=(const Fixed &other)
+{
+	std::cout << "Copy assignement operator called" << std::endl;
+	if (this != &other)
+		this->fixed_point = other.fixed_point;
+	return (*this);
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function calles" << std::endl;
+	return (this->fixed_point);
+}
+
+void	Fixed::setRawBits(const int raw)
+{
+	this->fixed_point = raw;
+}
